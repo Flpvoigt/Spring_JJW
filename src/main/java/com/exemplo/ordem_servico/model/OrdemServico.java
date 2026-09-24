2d77919322bbbdb5eaa4505c05afd4c44507b808
Flpvoigt <flpvoigt@gmail.com>
2026-09-24 11:09:54 -0300

    Desenvolvendo nova feature em dupla
    Co-authored-by: GustavoLoes <gustavoloes7@gmail.com>

--- src/main/java/com/exemplo/ordem_servico/model/OrdemServico.java
+++ src/main/java/com/exemplo/ordem_servico/model/OrdemServico.java
@@ -19,7 +19,7 @@ private static final double VALOR_HORA = 80.00;
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "O nome do cliente é obrigatório")
+    @NotBlank(message = "O nome do cliente é obrigatório!")
     private String cliente;
 
     @NotBlank(message = "A descrição do serviço é obrigatória")
